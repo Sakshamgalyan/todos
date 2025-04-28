07b6b2ff3ccc11bda0e8c7f3fcbb9b5f52c0d4bb
Sakshamgalyan <146658307+Sakshamgalyan@users.noreply.github.com>
2025-04-28 10:18:36 +0530

    ..

--- src/lib/mongodb.tsx
+++ src/lib/mongodb.tsx
@@ -1,7 +1,6 @@
 import { MongoClient } from 'mongodb';
 
 const url = process.env.DATABASE_URL;
-const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
@@ -10,12 +9,12 @@ if (!url) throw new Error('Add MONGODB_URI to .env.local');
 
 if (process.env.NODE_ENV === 'development') {
   if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(url, options);
+    client = new MongoClient(url);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  client = new MongoClient(url, options);
+  client = new MongoClient(url);
   clientPromise = client.connect();
 }
 
